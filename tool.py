e320d6c795a9d12dcc7d686f4919b8ecb7e33f14
IryVk <aa2101585@tkh.edu.eg>
2023-11-28 01:50:36 +0000

    yay

--- tool.py
+++ tool.py
@@ -5,7 +5,7 @@ def ssh_con(host, user, psswd):
     try:
         # add cipher and algorithm since ssh is strange on the isos
         paramiko.Transport._preferred_ciphers = ("aes256-cbc", )
-        paramiko.Transport._preferred_kex = ("diffie-hellman-group-exchange-sha1")
+        paramiko.Transport._preferred_kex = ("diffie-hellman-group-exchange-sha1", )
 
         # initialize ssh object thing
         ssh = paramiko.SSHClient()
@@ -13,7 +13,7 @@ def ssh_con(host, user, psswd):
         ssh.load_system_host_keys()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)    
 
-        ssh.connect(host, username=user, password=psswd)
+        ssh.connect(host, username=user, password=psswd, allow_agent=False, look_for_keys=False)
         print(f"Established ssh connection to {host}")
         return ssh
     except Exception as e:
